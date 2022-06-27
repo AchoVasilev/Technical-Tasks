145f2ed4af5f501347bed87c16b5dbd9ef4d6f8c
AngelVasilev <defiend@abv.bg>
2022-06-27 18:45:15 +0300

    Changed social contributions logic

--- Inensia tasks/SecondTask/App/Program.cs
+++ Inensia tasks/SecondTask/App/Program.cs
@@ -22,13 +22,18 @@
             // As for the previous case, the taxable income is whatever is above 1000 IDR but social contributions never apply to amounts higher than 3000. 
             // Example 2 : Irina has a salary of 3400 IDR. She owns income tax : 10% out of 2400 => 240. 
             // Her Social contributions are 15% out of 2000 => 300. In total her tax is 540 and she gets to bring home 2860 IDR
-            
-            // We don't tax any sum below 1000 and any sum higher than 3000. 
-            // The second example is not very clear, so I believe that the sums are 3400 and 3000, instead of 2400 and 2000.
-            if (grossValue > Constants.NonTaxableSum && grossValue <= Constants.SocialContributionMaxSumThreshold)
+            if (grossValue > Constants.NonTaxableSum)
             {
                 var socialContribution = new SocialContribution();
-                salary -= socialContribution.TaxCalculator.CalculateTax(Constants.SocialContributionMaxSumThreshold, socialContribution.Percentage);
+
+                if (grossValue <= Constants.SocialContributionMaxSumThreshold)
+                {
+                    salary -= socialContribution.TaxCalculator.CalculateTax(grossValue, socialContribution.Percentage);
+                }
+                else
+                {
+                    salary -= socialContribution.TaxCalculator.CalculateTax(Constants.SocialContributionMaxSumThreshold, socialContribution.Percentage);
+                }
             }
 
             Console.WriteLine(salary);
